0325b5d0009474b157aa926378d954ff53c8606b
gabrielmagno <gabrielmagno1@gmail.com>
2013-05-25 12:48:54 -0300

    Adicionado coleta de informações extras da linha

--- mgbus/mgbus.py
+++ mgbus/mgbus.py
@@ -1,33 +1,58 @@
+# -- encoding: utf-8 --
+
 import re
 import urllib
 
-def coleta_tabela_onibus_belohorizonte(linha):
-    html = urllib.urlopen("http://servicosbhtrans.pbh.gov.br/bhtrans/servicos_eletronicos/transporte_qh_resultado.asp?linha=%s" % (linha)).read()
-    sublinhas = re.findall("<td class=\"tituloTabela\"><a href=\"#\" ><img src=\"[^\"]+\" alt=\"[^\"]+\" border=\"0\" onclick=\"switchMenu\(\'([^\']+)\',this\);\"  /></a><b>&nbsp;([^<]+)</b> </td>", html, re.DOTALL)
-    sublinhas = [(sl_id, re.sub("[\n\r ]+", " ", sl_nome).strip()) for sl_id, sl_nome in sublinhas]
-    tabela_horarios = []
+def coleta_lista_linhas_belohorizonte():
+    html = urllib.urlopen("http://servicosbhtrans.pbh.gov.br/bhtrans/servicos_eletronicos/transporte_qh_info.asp").read()
+    linhas = re.findall("<a rel=\'external\' href=\"transporte_qh_resultado\.asp\?linha=([^\"]+)\" data-transition=\"slide\">", html)
+    return linhas
+
+def coleta_info_linha_belohorizonte(linha_busca):
+    
+    linha_info = {}
+
+    html = urllib.urlopen("http://servicosbhtrans.pbh.gov.br/bhtrans/servicos_eletronicos/transporte_qh_resultado.asp?linha=%s" % (linha_busca)).read()
+    
+    busca_linhaonibus = re.search("<div class=\"linhaOnibus\"> <img src=\"[^\"]+\" alt=\"[^\"]+\" class=\"left2\" />\s+<h1>Linha(.+?)<br />(.+?)</h1>", html, re.DOTALL)
+    busca_tarifa = re.search("<div class=\"descricao\">\s+<p><span>TARIFA: </span>R\$([^<]+)</p>", html, re.DOTALL)
+    busca_concessionario = re.search("<p><span>CONCESSION..RIO:</span></p>\s+<p>(.+?)</p>", html, re.DOTALL)
+    busca_sublinhas = re.findall("onclick=\"switchMenu\(\'([^\']+)\',this\);\"  /></a><b>&nbsp;([^<]+)</b> </td>", html, re.DOTALL)
+
+    linha_info["numero"] = busca_linhaonibus.group(1).strip() if busca_linhaonibus is not None else None
+    linha_info["nome"] = busca_linhaonibus.group(2).strip() if busca_linhaonibus is not None else None
+    linha_info["tarifa"] = float(busca_tarifa.group(1).strip().replace(",", ".")) if busca_tarifa is not None else None
+    linha_info["concessionario"] = re.sub("[\n\r ]+", " ", busca_concessionario.group(1).replace("&nbsp;", "")).replace("<br /> ", "\n").strip() if busca_concessionario is not None else None
+
+    linha_info["horarios"] = []
+    sublinhas = [(sl_id, re.sub("[\n\r ]+", " ", sl_nome).strip()) for sl_id, sl_nome in busca_sublinhas]
     for sublinha_id, sublinha_nome in sublinhas:
         sublinha_html = re.search("<div id=\"%s\" style=\" display:none\" >(.*?)</table>" % (sublinha_id), html, re.DOTALL).group(1)
         horas = map(int, re.findall("<td class=\"celHoras\" > (\d+) </td>", sublinha_html))
         horas_minutos_html = re.findall("<td width=\'4%\' class=\'celMinutos\'>(.*?)</td>", sublinha_html)
-        horarios = []
+        sublinha_horarios = []
         for hora, minutos_html in zip(horas, horas_minutos_html[1:]):
             minutos = re.findall("<a (class=\'color\' )?href=\'[^\']+\'>(\d+)</a>", minutos_html)
             minutos_dados = []
             for tipo, minuto in minutos:
                 minutos_dados.append([int(minuto), (True if len(tipo) > 0 else False)])
-            horarios.append([hora, minutos_dados])
-        tabela_horarios.append([sublinha_nome, horarios])
-    return tabela_horarios
+            sublinha_horarios.append([hora, minutos_dados])
+        linha_info["horarios"].append([sublinha_nome, sublinha_horarios])
+
+    return linha_info
 
 
-def imprime_horarios_onibus_belo_horizonte(linha):
-    tabela_horarios = coleta_tabela_onibus_belohorizonte(linha)
-    for sublinha_nome, horarios in tabela_horarios:
-        print "+ %s" % (sublinha_nome)
+def imprime_horarios_onibus_belo_horizonte(linha_busca):
+    linha_info = coleta_info_linha_belohorizonte(linha_busca)
+    print "* %s" % (linha_busca)
+    print "    + Numero: %s" % (linha_info["numero"])
+    print "    + Nome: %s" % (linha_info["nome"])
+    print "    + Horarios:"
+    for sublinha_nome, horarios in linha_info["horarios"]:
+        print "        - %s" % (sublinha_nome)
         for hora, minutos_dados in horarios:
             if len(minutos_dados) > 0:
-                print "    -",
+                print "            |",
                 for minuto, tipo in minutos_dados:
                     if tipo:
                         print "%d:%d*" % (hora, minuto),
@@ -35,6 +60,9 @@ def imprime_horarios_onibus_belo_horizonte(linha):
                         print "%d:%d" % (hora, minuto),
                 print
 
+#print coleta_lista_linhas_belohorizonte()
+#print coleta_info_linha_belohorizonte("S54")
 imprime_horarios_onibus_belo_horizonte("4405")
+imprime_horarios_onibus_belo_horizonte("2402A")
 imprime_horarios_onibus_belo_horizonte("S54")
-imprime_horarios_onibus_belo_horizonte("SC02")
+
